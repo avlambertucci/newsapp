b669d40f77caa3ed28db7d46cb028615c3ac3e4e
avlambertucci <armandolambertucci@gmail.com>
2020-11-29 20:53:32 -0300

    app done

--- newsapp/__init__.py
+++ newsapp/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
+
 from .extensions import mongo
 from .main.routes import main
 
 
 def create_app():
   app = Flask(__name__)
-
+  app.secret_key = 'many random bytes'
   app.config['MONGO_URI'] = 'mongodb+srv://admin:182182@cluster0.cxaae.mongodb.net/mydb?retryWrites=true&w=majority'
-  mongo.init_app(app)
+  mongo.init_app(app) 
 
   app.register_blueprint(main)
 
--- newsapp/main/routes.py
+++ newsapp/main/routes.py
@@ -1,7 +1,72 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from newsapp.extensions import mongo
+from bson.objectid import ObjectId
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
 def index():
-  return render_template('index.html')
+  news_collection = mongo.db.news
+  news = news_collection.find()
+  return render_template('index.html', news=news)
+
+@main.route('/add_new', methods=["POST"])
+def add_new():
+
+  news_collection = mongo.db.news
+  titulo = request.form.get('titulo')
+  texto = request.form.get('texto')
+  autor = request.form.get('autor')
+  new = {
+    "titulo": titulo,
+    "texto": texto,
+    "autor": autor,
+    "deleted": False 
+  }
+
+  flash("A notícia foi adicionada")
+  news_collection.insert_one(new)
+  return redirect(url_for('main.index'))
+
+@main.route('/delete_new/<oid>')
+def delete_new(oid):
+  news = mongo.db.news
+  news.remove({"_id": ObjectId(oid)});
+  return redirect(url_for('main.index'))
+  # return render_template('index.html', news=news)
+
+@main.route('/search_new', methods=["POST", "GET"])
+def search_new():
+  
+  news_collection = mongo.db.news
+  search_titulo = request.form.get('search_titulo')
+  one_new = news_collection.find_one({'titulo': search_titulo})
+  print(search_titulo)
+  if(one_new):
+    array = [one_new]
+    return render_template('index.html', news=array)
+  
+  flash("Nenhum usuario encontrado")
+  return redirect(url_for('main.index'))
+  # return render_template('index.html', news=one_new)
+
+@main.route('/update_new/<id>', methods=["POST", "GET"])
+def update_new(id):
+  news = mongo.db.news
+  item_id_to_update = {"_id": ObjectId(id)}
+  titulo = request.form.get('titulo')
+  texto = request.form.get('texto')
+  autor = request.form.get('autor')
+  new_values = {
+    "titulo": titulo,
+    "texto": texto,
+    "autor": autor,
+    "deleted": False 
+  }
+
+  news.update(item_id_to_update, new_values)
+  return redirect(url_for('main.index'))
+  
+  # return render_template('index.html', news=news)
+ 
